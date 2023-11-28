4c00cd9c424f50a21ade00e6841700c0923913e8
Vladbazuka <zemskiy2002@mail.ru>
2023-11-28 19:28:00 +0300

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+const browserSync = require('browser-sync');
 const { src, dest, watch, parallel} = require('gulp');
 
 const sass   = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
 
 
 function scripts() {
@@ -10,6 +12,7 @@ function scripts() {
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('build/js'))
+    .pipe(browserSync.stream())
 }
 
 function style () {
@@ -17,6 +20,7 @@ function style () {
         .pipe(concat('style.min.css'))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(dest('build/style'))
+        .pipe(browserSync.stream())
 }
 
 function html() {
@@ -26,11 +30,21 @@ function html() {
 function watching() {
     watch(['src/style/style.scss'], style)
     watch(['src/js/main.js'], scripts)
+    watch(['src/*html']).on('change', browserSync.reload);
 } 
+
+function browsersync(){
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+}
 
 exports.html = html;
 exports.style = style;
 exports.scripts = scripts;
 exports.watching = watching;
+exports.browsersync = browsersync;
 
 exports.default =  parallel(html,style,scripts,watching);
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 const box = document. querySelector('box');
 const btm = document. querySelector('.button');
+const menu = document. querySelector('.menu');
