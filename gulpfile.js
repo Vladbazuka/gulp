e4d8ea0d5f20dc7ac80e95b180cb41633f6d04bd
Vladbazuka <zemskiy2002@mail.ru>
2023-11-28 17:47:42 +0300

    установка concat, доработка функций стилей и js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const { src, dest} = require('gulp');
 
-const sass = require('gulp-sass')(require('sass'));
+const sass   = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
 
+
+function scripts() {
+    return src('src/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('build/js'))
+}
 
 function style () {
     return src('src/style/style.scss')
+        .pipe(concat('style.min.css'))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(dest('build/style'))
 }
@@ -17,3 +27,4 @@ function html() {
 
 exports.html = html;
 exports.style = style;
+exports.scripts = scripts;
